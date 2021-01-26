3aabf582d8711d5d487600fea913627760bb57d8
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-01-26 16:52:22 +0300

    update

--- codo/Zadachi/arifmetic.py
+++ codo/Zadachi/arifmetic.py
@@ -7,6 +7,7 @@ minuts = b % 60
 print(b, "мин - это", hours, "часов", minuts, "минут")
 print(f"{b} мин - это {hours} часов {minuts} минут")
 print(str(b) + " мин - это " + str(hours) + " часов " + str(minuts) + " минут")
-os.system('cls||clear')
+print('\r\x1b[K', end='')
+#os.system('cls||clear')
 print("{} мин - это {} часов {} минут".format(b, hours, minuts))
 print("%d мин - это %d часов %d минут" % (b, hours, minuts))
