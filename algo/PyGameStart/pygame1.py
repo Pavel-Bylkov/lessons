1f8347d39353e11cb46036c7648611066044a247
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-03-13 20:17:35 +0300

    update start game

--- algo/PyGameStart/pygame1.py
+++ algo/PyGameStart/pygame1.py
@@ -5,26 +5,18 @@ win_widht, win_height = 700, 500
 window = display.set_mode((win_widht, win_height))
 display.set_caption("Догонялки")
 
-background = transform.scale(
-    image.load("/home/whector/Documents/GitHub/lessons/algo/PyGameStart/background.png"),
-    (win_widht, win_height))
-final = transform.scale(
-    image.load("/home/whector/Documents/GitHub/lessons/algo/PyGameStart/thumb_1.jpg"),
-    (win_widht, win_height))
+background = transform.scale(image.load("background.png"), (win_widht, win_height))
+
 sp1_widht, sp1_height = 50, 50
 x1, y1 = 100, 300
-speed = 5
-sprite1 = transform.scale(
-    image.load('/home/whector/Documents/GitHub/lessons/algo/PyGameStart/sprite1.png'), 
-    (sp1_widht, sp1_height))
+speed = 3
+sprite1 = transform.scale(image.load('sprite1.png'), (sp1_widht, sp1_height))
 
 sp2_widht, sp2_height = 50, 50
 x2, y2 = 300, 400
 new_x2, new_y2 = x2, y2
-speed2 = 3
-sprite2 = transform.scale(
-    image.load('/home/whector/Documents/GitHub/lessons/algo/PyGameStart/sprite2.png'), 
-    (sp2_widht, sp2_height))
+speed2 = 6
+sprite2 = transform.scale(image.load('sprite2.png'), (sp2_widht, sp2_height))
 
 clock = time.Clock()
 FPS = 60
@@ -36,23 +28,31 @@ def is_touching():
 
 def get_new_pos_sp2():
     global new_x2, new_y2
-    while abs(new_x2 - x2) < 3 * sp2_widht and abs(new_y2 - y2) < 3 * sp2_height:
-       new_x2, new_y2 = randint(1, 65) * 10, randint(1, 45) * 10
+    new_x2, new_y2 = randint(0, 65) * 10, randint(1, 45) * 10
+    while (abs(x1 - x2) > abs(x1 - new_x2) and abs(y1 - y2) > abs(y1 - new_y2)):
+        new_x2, new_y2 = randint(0, 65) * 10, randint(1, 45) * 10
 
-def enemy_move(x2, y2, new_x2, new_y2):
-    if new_x2 == x2 and new_y2 == y2:
+def enemy_move():
+    global x2, y2, new_x2, new_y2
+    if x2 in tuple(range(new_x2 - 10, new_x2 + 10)) and y2 in tuple(range(new_y2 - 10, new_y2 + 10)):
         get_new_pos_sp2()
+    x, y = x2, y2
+    dist_x = abs(x1 - x2)
+    dist_y = abs(y1 - y2)
     if x2 > new_x2:
        x2 -= speed2
-    elif x2 < new_x2:
+    if x2 < new_x2:
        x2 += speed2
     if y2 > new_y2:
        y2 -= speed2
-    elif y2 < new_y2:
+    if y2 < new_y2:
        y2 += speed2
-    return x2, y2
+    if dist_x >= abs(x1 - x2) and dist_y >= abs(y1 - y2):
+        x2, y2 = x, y
+        get_new_pos_sp2()
 
-def control(x1, y1):
+def control():
+    global x1, y1
     keys_pressed = key.get_pressed()
     if keys_pressed[K_LEFT] and x1 > 5:
         x1 -= speed
@@ -62,7 +62,6 @@ def control(x1, y1):
         y1 -= speed
     if keys_pressed[K_DOWN] and y1 < 450:
         y1 += speed
-    return x1, y1
 
 run = True
 while run:
@@ -70,8 +69,8 @@ while run:
         if e.type == QUIT:
           run = False
     if not is_touching():
-        x1, y1 = control(x1, y1)
-        x2, y2 = enemy_move(x2, y2, new_x2, new_y2)
+        control()
+        enemy_move()
     window.blit(background, (0, 0))
     window.blit(sprite1, (x1, y1))
     window.blit(sprite2, (x2, y2))
