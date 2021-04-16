da9c5b28b4dd5f49034c3d87d7cedeb449f757f0
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-04-16 17:30:41 +0300

    final fast clicker

--- algo/PyGameStart/fast_clicker.py
+++ algo/PyGameStart/fast_clicker.py
@@ -2,6 +2,9 @@ import pygame
 import time
 from random import randint
 pygame.init()
+
+limit_time = 10
+goal = 5
  
 '''создаём окно программы'''
 back = (200, 255, 255) #цвет фона (background)
@@ -15,15 +18,12 @@ class Area():
    def __init__(self, x, y, width, height, color):
        self.rect = pygame.Rect(x, y, width, height) #прямоугольник
        self.fill_color = color
- 
    def color(self, new_color):
        self.fill_color = new_color
- 
    def fill(self):
        pygame.draw.rect(mw, self.fill_color, self.rect)
    def outline(self, frame_color, thickness): #обводка существующего прямоугольника
        pygame.draw.rect(mw, frame_color, self.rect, thickness)    
- 
    def collidepoint(self, x, y):
        return self.rect.collidepoint(x, y)       
  
@@ -31,7 +31,6 @@ class Area():
 class Label(Area):
    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
- 
    def draw(self, shift_x=0, shift_y=0):
        self.fill()
        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
@@ -77,27 +76,29 @@ lose.set_text(text='Время вышло!!!',fsize=90, text_color=DARK_BLUE)
 wait = 0
 last_click = 0
 run = True
+finish = False
 while run:
-    if wait == 0:
-        #переносим надпись:
-        wait = 26 #столько тиков надпись будет на одном месте
-        click = randint(0, num_cards - 1)
-        while last_click == click:
+    if not finish:
+        if wait == 0:
+            #переносим надпись:
+            wait = 26 #столько тиков надпись будет на одном месте
             click = randint(0, num_cards - 1)
-        last_click = click
-        for i in range(num_cards):
-            cards[i].color(new_color=YELLOW)
-            if i == click:
-                cards[i].draw(shift_x=10, shift_y=40)
-            else:
-                cards[i].fill()
-    else:
-        wait -= 1
+            while last_click == click:
+                click = randint(0, num_cards - 1)
+            last_click = click
+            for i in range(num_cards):
+                cards[i].color(new_color=YELLOW)
+                if i == click:
+                    cards[i].draw(shift_x=10, shift_y=40)
+                else:
+                    cards[i].fill()
+        else:
+            wait -= 1
     #на каждом тике проверяем клик:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  #обработай событие «клик по кнопке "Закрыть окно"»
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if not finish and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             x, y = event.pos
             for i in range(num_cards):
                 #ищем, в какую карту попал клик
@@ -111,16 +112,21 @@ while run:
                         if score.num > 0:
                             score.num -= 1
                         cards[i].fill()
-    
-    score.set_text(text=str(score.num), fsize=40, text_color=DARK_BLUE)
-    score.draw(shift_x=0, shift_y=0)
-    timer.curent_time = time.time() - timer.start_time
-    timer.set_text(text=str(int(timer.curent_time)), fsize=40, text_color=DARK_BLUE)
-    timer.draw(shift_x=0, shift_y=0)
-    
-    if timer.curent_time > 10 and score.num < 5:    # Условие Поражения
-        lose.draw(shift_x=100, shift_y=170)
-    elif score.num >= 5:                            # Условие Победы
-        win.draw(shift_x=200, shift_y=170)
-    pygame.display.update()
+    if not finish:
+        score.set_text(text=str(score.num), fsize=40, text_color=DARK_BLUE)
+        score.draw(shift_x=0, shift_y=0)
+        timer.curent_time = time.time() - timer.start_time
+        timer.set_text(text=str(int(timer.curent_time)), fsize=40, text_color=DARK_BLUE)
+        timer.draw(shift_x=0, shift_y=0)
+        
+        if timer.curent_time > limit_time and score.num < goal:    # Условие Поражения
+            lose.draw(shift_x=100, shift_y=170)
+            finish = True
+        elif score.num >= goal:                            # Условие Победы
+            win.draw(shift_x=200, shift_y=170)
+            resul_time = Label(170, 300, 250, 250, GREEN)
+            resul_time.set_text("Время прохождения: " + str(int(timer.curent_time)) + " сек", 40, DARK_BLUE)
+            resul_time.draw(0, 0)
+            finish = True
+        pygame.display.update()
     clock.tick(40)           
