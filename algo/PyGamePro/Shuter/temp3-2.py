f87174d8805d1a17280b6456abeb79caba95c056
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-05-03 17:02:10 +0300

    update shuter

--- algo/PyGamePro/Shuter/temp3-2.py
+++ algo/PyGamePro/Shuter/temp3-2.py
@@ -1,5 +1,5 @@
-from pygame import (sprite, display, time, transform, image, mixer, event, key, 
-                        QUIT, KEYDOWN, K_SPACE, K_LEFT, K_RIGHT)
+from pygame import (init, sprite, display, time, transform, image, mixer, event, key, font, 
+                        QUIT, KEYDOWN, K_SPACE, K_LEFT, K_RIGHT, K_p)
 from random import randint
 from time import time as time_t
 img_back = "galaxy.jpg" # фон игры
@@ -11,10 +11,15 @@ img_boom = "Взрыв4.png"
 sound_fon = "space.ogg"
 sound_fire = "laser-blast.ogg"
 
+WHITE = (255, 255, 255)
+GREEN = (50, 200, 50)
+RED = (200, 50, 50)
+
 score = 0  # сбито кораблей
 goal = 15  # столько кораблей нужно сбить для победы
 lost = 0  # пропущено кораблей
 max_lost = 5  # проиграли, если пропустили столько
+life = 3
 
 # класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
@@ -95,11 +100,21 @@ class Boom(sprite.Sprite):
         elif self.index == len(self.imgs):
             self.kill()
 
+def text_update(text, num, position):
+    window.blit(font2.render(text + str(num), True, WHITE), position)
+
 # Создаем окошко
-win_width, win_height = 800, 500
+win_width, win_height = 1200, 800
 display.set_caption("Shuter")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
+init() # Инициализация (настройка) Pygame
+font.init()  # Инициализация модуля font
+font1 = font.SysFont("Arial", 100)  # Создание объекта Шрифт
+font2 = font.SysFont("Arial", 30) 
+win = font1.render("YOU WIN!!!", True, GREEN)  # Создание картинки из текста
+lose = font1.render("YOU LOSE!!!", True, RED)
+pause_img = font1.render("PAUSE", True, WHITE)
 # создаем спрайты
 ship = Player(img_hero, x=win_width//2, y=win_height - 100,
                             size_x=60, size_y=80, speed=10)
@@ -122,6 +137,7 @@ fire_sound = mixer.Sound(sound_fire)
 fire_sound.set_volume(0.3)
 
 finish = False
+pause = False
 # Основной цикл игры:
 run = True # флаг сбрасывается кнопкой закрытия окна
 while run:
@@ -133,7 +149,12 @@ while run:
         if e.type == KEYDOWN and e.key == K_SPACE:
             fire_sound.play()
             ship.fire()
-    if not finish:
+        if e.type == KEYDOWN and e.key == K_p:
+            if pause:
+                pause = False
+            else:
+                pause = True
+    if not finish and not pause:
         # обновляем фон
         window.blit(background,(0,0))
         # производим движения спрайтов
@@ -147,6 +168,10 @@ while run:
         bullets.draw(window)
         booms.draw(window)
 
+        text_update(text="Подбито: ", num=score, position=(10, 20))
+        text_update(text="Пропущено: ", num=lost, position=(10, 50))
+        text_update(text="Жизни: ", num=life, position=(10, 80))
+
         collides = sprite.groupcollide(monsters, bullets, False, True)
         for c in collides:
             score += 1
@@ -155,7 +180,26 @@ while run:
             c.rect.x = randint(10, win_width - 80)
             c.rect.y = randint(-50, -4)*10
             c.speed = randint(1, 5)
+        collides = sprite.spritecollide(ship, monsters, False)
+        if life > 0 and collides:
+            life -= 1
+            for c in collides:
+                boom = Boom(x=c.rect.centerx, y=c.rect.centery, size_x=c.rect.width, size_y=c.rect.height)
+                booms.add(boom)
+                c.rect.x = randint(10, win_width - 80)
+                c.rect.y = randint(-50, -4)*10
+                c.speed = randint(1, 5)
 
-        display.update()
+        if score >= goal:
+            finish = True
+            final_txt = win
+        if lost >= max_lost or life == 0:
+            finish = True
+            final_txt = lose
+    elif not pause:
+        window.blit(final_txt, (win_width//2 - 200, win_height//2 - 50))
+    else:
+       window.blit(pause_img, (win_width//2 - 200, win_height//2 - 50)) 
+    display.update()
     # цикл срабатывает каждую 0.05 секунд
     time.delay(50)
