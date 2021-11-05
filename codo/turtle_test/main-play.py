cf7c1c3d6501b81d29f28c2748637eb0fa4b346c
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-11-05 21:34:31 +0300

    add lines

--- codo/turtle_test/main-play.py
+++ codo/turtle_test/main-play.py
@@ -2,12 +2,6 @@
 
 import play
 
-box = play.new_box(
-        color='black', x=0, y=0,
-        width=30, height=30,
-        border_color="light blue",
-        border_width=1
-    )
 
 fon = play.new_image(
         image='labirint.jpeg',
@@ -30,11 +24,68 @@ lines = [
     #     angle=90, thickness=5, x1=None, y1=None),
     # play.new_line(color='blue', x=-285, y=150, length=65,
     #     angle=0, thickness=5, x1=None, y1=None),
-    play.new_line(color='blue', x=-160, y=150, length=65,
-        angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-160, y=150, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-35, y=150, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-160, y=-95, length=60,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-160, y=-220, length=60,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-285, y=25, length=125,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=30, y=25, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=210, y=25, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=95, y=90, length=125,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-225, y=90, length=125,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-100, y=215, length=195,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-160, y=-35, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-35, y=-35, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=155, y=-95, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-95, y=-95, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-220, y=-160, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-35, y=-160, length=65,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=90, y=-160, length=190,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=-95, y=-220, length=310,
+    #     angle=0, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=215, y=90, length=130,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=215, y=-95, length=125,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=155, y=155, length=125,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=155, y=-30, length=125,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=90, y=90, length=125,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=90, y=-160, length=190,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=30, y=25, length=130,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    # play.new_line(color='blue', x=30, y=-220, length=185,
+    #     angle=90, thickness=5, x1=None, y1=None),
+    play.new_line(color='blue', x=-35, y=-35, length=185,
+        angle=90, thickness=5, x1=None, y1=None),
 ]
 
-
+box = play.new_box(
+        color='black', x=-240, y=240,
+        width=30, height=30,
+        border_color="light blue",
+        border_width=1
+    )
 
 @play.repeat_forever
 async def move_box():
