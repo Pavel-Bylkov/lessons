355ed654f7dcd493982567562472a55c82f6a74c
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-11-17 20:14:06 +0300

    update

--- codo/turtle_test/main-play.py
+++ codo/turtle_test/main-play.py
@@ -2,6 +2,8 @@
 
 import play
 
+from config import param_lines
+
 # todo сделать непроходимыми стены
 
 # fon = play.new_image(
@@ -11,48 +13,7 @@ import play
 #     )
 
 # todo скорректировать координаты линий и длины - убрать хвосты
-# в списке храним параметры построения линий лабиринта
-param_lines = [
-    {"x": -285, "y": 280, "length": 565, "angle": 0},
-    {"x": -285, "y": -285, "length": 560, "angle": 90},
-    {"x": -285, "y": -285, "length": 565, "angle": 0},
-    {"x": 280, "y": -280, "length": 560, "angle": 90},
-    {"x": -160, "y": 220, "length": 60, "angle": 90},
-    {"x": -285, "y": 150, "length": 65, "angle": 0},
-    {"x": -160, "y": 150, "length": 65, "angle": 0},
-    {"x": -35, "y": 150, "length": 65, "angle": 0},
-    {"x": -160, "y": -95, "length": 60, "angle": 90},
-    {"x": -160, "y": -220, "length": 60, "angle": 90},
-    {"x": -285, "y": 25, "length": 125, "angle": 0},
-    {"x": 30, "y": 25, "length": 65, "angle": 0},
-    {"x": 210, "y": 25, "length": 65, "angle": 0},
-    {"x": 95, "y": 90, "length": 125, "angle": 0},
-    {"x": -225, "y": 90, "length": 125, "angle": 0},
-    {"x": -100, "y": 215, "length": 195, "angle": 0},
-    {"x": -160, "y": -35, "length": 65, "angle": 0},
-    {"x": -35, "y": -35, "length": 65, "angle": 0},
-    {"x": 155, "y": -95, "length": 65, "angle": 0},
-    {"x": -95, "y": -95, "length": 65, "angle": 0},
-    {"x": -220, "y": -160, "length": 65, "angle": 0},
-    {"x": -35, "y": -160, "length": 65, "angle": 0},
-    {"x": 90, "y": -160, "length": 190, "angle": 0},
-    {"x": -95, "y": -220, "length": 310, "angle": 0},
-    {"x": 215, "y": 90, "length": 130, "angle": 90},
-    {"x": 215, "y": -95, "length": 125, "angle": 90},
-    {"x": 155, "y": 155, "length": 125, "angle": 90},
-    {"x": 155, "y": -30, "length": 125, "angle": 90},
-    {"x": 90, "y": 90, "length": 125, "angle": 90},
-    {"x": 90, "y": -160, "length": 190, "angle": 90},
-    {"x": 30, "y": 25, "length": 130, "angle": 90},
-    {"x": 30, "y": -220, "length": 185, "angle": 90},
-    {"x": -35, "y": -35, "length": 185, "angle": 90},
-    {"x": -90, "y": -225, "length": 315, "angle": 90},
-    {"x": -95, "y": 150, "length": 65, "angle": 90},
-    {"x": -215, "y": -280, "length": 65, "angle": 90},
-    {"x": -215, "y": -165, "length": 190, "angle": 90}
-    ]
-
-lines = [play.new_line(color='blue', **args, thickness=5, x1=None, y1=None)
+lines = [play.new_line(color='blue', **args, thickness=8, x1=None, y1=None)
          for args in param_lines]
 
 box = play.new_box(
@@ -64,17 +25,24 @@ box = play.new_box(
 
 @play.repeat_forever
 async def move_box():
+
+    old_x, old_y = box.x, box.y
     if play.key_is_pressed('up', 'w'):
-        box.y += 15
-    if play.key_is_pressed('down', 's'):
-        box.y -= 15
+        box.y += 5   # y = y + 15   y += 15
+    elif play.key_is_pressed('down', 's'):
+        box.y -= 5
 
     if play.key_is_pressed('right', 'd'):
-        box.x += 15
-    if play.key_is_pressed('left', 'a'):
-        box.x -= 15
+        box.x += 5
+    elif play.key_is_pressed('left', 'a'):
+        box.x -= 5
 
-    await play.timer(seconds=0.01)
+    if box.x != old_x or box.y != old_y:
+        for line in lines:
+            if box.is_touching(line):
+                box.x, box.y = old_x, old_y
+
+    await play.timer(seconds=0.001)
 
 
 # @play.mouse.when_clicked
