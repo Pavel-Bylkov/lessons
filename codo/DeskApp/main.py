43bdd05ecf64c1eddbd0c7f50caceb21effc82a5
Pavel-Bylkov <oldman.bpv@gmail.com>
2022-01-26 19:35:15 +0300

    update

--- codo/DeskApp/main.py
+++ codo/DeskApp/main.py
@@ -1,7 +1,8 @@
 # https://pythonworld.ru/gui/pyqt5-firstprograms.html
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,
-                            QPushButton)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout,
+                            QPushButton, QToolTip)
+from PyQt5.QtGui import QFont
 
 # создаем приложение
 app = QApplication([])
@@ -14,10 +15,20 @@ win.setWindowTitle('Мое приложение')
 label = QLabel("Надпись")
 button = QPushButton("OK")
 
+QToolTip.setFont(QFont('Arial', 10))
+
+win.setToolTip('This is a <b>QWidget</b> widget')
+button.setToolTip('This is a <b>QPushButton</b> widget')
+
+
 # вертикальная линия для привязки
 vertical_line = QVBoxLayout()
-vertical_line.addWidget(label)
-vertical_line.addWidget(button)
+# горизонтальная линия для привязки
+horizontal_line = QHBoxLayout()
+horizontal_line.addWidget(label, alignment=Qt.AlignCenter)
+# связываем две линии - к вертикальной добавляем горизонтальную
+vertical_line.addLayout(horizontal_line)
+vertical_line.addWidget(button, alignment=Qt.AlignCenter)
 
 # привязываем линию с надписью к окну
 win.setLayout(vertical_line)
