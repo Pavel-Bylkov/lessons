ea69e2f5d7a2e2990c827c41a8175ea44322cc88
Pavel-Bylkov <oldman.bpv@gmail.com>
2022-03-09 19:48:32 +0300

    add Controller

--- codo/DeskApp/game2.py
+++ codo/DeskApp/game2.py
@@ -6,6 +6,22 @@ from PyQt5.QtGui import QFont, QIntValidator
 from random import randint
 
 
+class Controller:
+    def __init__(self):
+        self.numbers = [str(x) for x in range(1, 16)]  # Создаем список чисел от 1 до 15
+        self.numbers.append(" ")
+        # print(self.numbers)
+        self.i = 0
+
+    def get_number(self):
+        if self.i < len(self.numbers):
+            value = self.numbers[self.i]
+            self.i += 1
+            return value
+        self.i = 0
+        return "stop"
+
+
 class MyWin(QWidget):
     def __init__(self, *args, **kwargs):
         # вызываем родительский конструктор
@@ -13,6 +29,7 @@ class MyWin(QWidget):
         self.resize(600, 700)
         self.setWindowTitle('Пятнашки')
         self.buttons = []
+        self.controller = Controller()
         self.create_widgets()
 
     def create_widgets(self):
@@ -24,7 +41,7 @@ class MyWin(QWidget):
         for row in range(4):
             horizontal_line = QHBoxLayout()
             for column in range(4):
-                button = MyButton(row, column)
+                button = MyButton(row, column, self.controller.get_number())
                 self.buttons.append(button)
                 horizontal_line.addWidget(button)  # , alignment=Qt.AlignCenter
             vertical_line.addLayout(horizontal_line)
@@ -33,13 +50,13 @@ class MyWin(QWidget):
 
 class MyButton(QPushButton):
     # переопределяем конструктор класса
-    def __init__(self, row, column, *args, **kwargs):
+    def __init__(self, row, column, text, *args, **kwargs):
         # вызываем родительский конструктор
         super().__init__(*args, **kwargs)
         # переопределяем значения по умолчанию
         self.setFixedSize(QSize(148, 148))
         self.setFont(QFont('Arial', 100))
-        self.setText(" ")
+        self.setText(text)
         self.pos = (row, column)
         # привязываем действие при нажатии на кнопку
         self.clicked.connect(self.push)
@@ -50,6 +67,7 @@ class MyButton(QPushButton):
         #     self.setText(turn_char)
         #     game_controller(self.pos)
 
+
 def main():
     # создаем приложение
     app = QApplication([])
@@ -58,5 +76,6 @@ def main():
     win.show()
     # главный цикл
     app.exec_()
+
 
 main()
--- codo/DeskApp/test_print.py
+++ codo/DeskApp/test_print.py
@@ -1,5 +1,12 @@
-num = 55
-print("Text", num, "text2", num + 1)
-print(f"Text {num} text2 {num + 1}")
-print("Text {} text2 {}".format(num, num + 1))
-print("Text %d text2 %d" % (num, num + 1))
+# num = 55
+# print("Text", num, "text2", num + 1)
+# print(f"Text {num} text2 {num + 1}")
+# print("Text {} text2 {}".format(num, num + 1))
+# print("Text %d text2 %d" % (num, num + 1))
+
+# print(2 // 2 == 0)  # False = 0 | ""
+#
+# if 1 / 2 == 0:
+#     print("Ветка 1")
+# else:
+#     print("Ветка 2")
