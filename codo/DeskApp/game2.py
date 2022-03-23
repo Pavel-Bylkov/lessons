79ec5ebddff4b73cd6bee2f76a23fb59176eb227
Pavel-Bylkov <oldman.bpv@gmail.com>
2022-03-23 19:46:33 +0300

    update

--- codo/DeskApp/game2.py
+++ codo/DeskApp/game2.py
@@ -23,6 +23,11 @@ class Controller:
     def check_win(self):
         return self.numbers == self.result
 
+    def swap_number(self, row1, col1, row2, col2):
+        bufer = self.numbers[row2][col2]
+        self.numbers[row2][col2] = self.numbers[row1][col1]
+        self.numbers[row1][col1] = bufer
+
     @staticmethod
     def get_numbers(list_num):
         iter_l = iter(list_num)
@@ -55,18 +60,44 @@ class MyWin(QWidget):
             horizontal_line = QHBoxLayout()
             for column in range(4):
                 text = self.controller.get_number(row, column)
-                button = MyButton(row, column, text)
+                button = MyButton(row, column, text, self)
                 self.buttons.append(button)
                 horizontal_line.addWidget(button)  # , alignment=Qt.AlignCenter
             vertical_line.addLayout(horizontal_line)
         self.setLayout(vertical_line)
 
+    def get_btn(self, row, col):
+        for btn in self.buttons:
+            if btn.pos == (row, col):
+                return btn
+        return None
+
+    def get_btn_space(self, list_neib):
+        for btn in self.buttons:
+            if btn.pos in list_neib and btn.text() == ' ':
+                return btn
+        return None
+
+    def swap(self, row, col, list_neib):
+        btn1 = self.get_btn(row, col)  # получаем ссылку на кнопку куда кликнули
+        btn2 = self.get_btn_space(list_neib)  # получаем ссылку на кнопку с пустой ячейкой
+        if btn1 is not None and btn2 is not None:
+            row2, col2 = btn2.pos
+            self.controller.swap_number(row, col, row2, col2)
+            text = self.controller.get_number(row, col)
+            btn1.setText(text)
+            text = self.controller.get_number(row2, col2)
+            btn2.setText(text)
+            if self.controller.check_win():
+                print("Finish!!!")
+
 
 class MyButton(QPushButton):
     # переопределяем конструктор класса
-    def __init__(self, row, column, text, *args, **kwargs):
+    def __init__(self, row, column, text, main_win, *args, **kwargs):
         # вызываем родительский конструктор
         super().__init__(*args, **kwargs)
+        self.main_win = main_win  # сохраняем ссылку на главное окно
         # переопределяем значения по умолчанию
         self.setFixedSize(QSize(148, 148))
         self.setFont(QFont('Arial', 100))
@@ -84,18 +115,12 @@ class MyButton(QPushButton):
         start1 = 1 if self.pos[1] > 0 else 0
         end0 = 1 if self.pos[0] < 3 else 0
         end1 = 1 if self.pos[1] < 3 else 0
+        list_neib = []
         for row in range(self.pos[0] - start0, self.pos[0] + end0 + 1):
             for col in range(self.pos[1] - start1, self.pos[1] + end1 + 1):
-                if ((row, col) != self.pos
-                        and not (self.pos[0] - start0 == row and self.pos[1] - start1 == col)
-                        and not (self.pos[0] + end0 == row and self.pos[1] - start1 == col)
-                        and not (self.pos[0] - start0 == row and self.pos[1] + end1 == col)
-                        and not (self.pos[0] + end0 == row and self.pos[1] + end1 == col)):
-                    print(row, col)
-
-        # if self.text() == " ":
-        #     self.setText(turn_char)
-        #     game_controller(self.pos)
+                if (row, col) != self.pos and (self.pos[0] == row or self.pos[1] == col):
+                    list_neib.append((row, col))
+        self.main_win.swap(self.pos[0], self.pos[1], list_neib)
 
 
 def main():
