47532c7bc8f5243c0289671b34918fa23b529877
Pavel-Bylkov <oldman.bpv@gmail.com>
2022-03-30 19:43:37 +0300

    add figure

--- codo/GamesArcade/game1.py
+++ codo/GamesArcade/game1.py
@@ -1,5 +1,5 @@
 import arcade
-from arcade.color import BLACK, GREEN, WHITE, YELLOW, DARK_GREEN
+from arcade.color import *
 
 WIDTH = 800
 HEIGHT = 600
@@ -14,8 +14,23 @@ arcade.start_render()
 # рисуем линию
 arcade.draw_line(start_x=50, start_y=50, end_x=750, end_y=50,
                  color=YELLOW, line_width=3)
+# рисуем заполненный круг
+arcade.draw_circle_filled(center_x=WIDTH//2, center_y=HEIGHT//2,
+                          radius=50, color=RED, tilt_angle=0,
+                          num_segments=-1)
+arcade.draw_circle_filled(center_x=WIDTH//2, center_y=HEIGHT//2,
+                          radius=40, color=GREEN, tilt_angle=0,
+                          num_segments=5)
+arcade.draw_circle_filled(center_x=WIDTH//2, center_y=HEIGHT//2,
+                          radius=30, color=BLUE, tilt_angle=45,
+                          num_segments=5)
+# рисуем пустой круг
+arcade.draw_circle_outline(center_x=150, center_y=150, radius=50,
+                           color=BLACK, border_width=5, tilt_angle=0,
+                           num_segments=-1)
+
 # заканчиваем рисование
 arcade.finish_render()
 
-
+# запускаем игровой цикл
 arcade.run()
